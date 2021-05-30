10f524c58b1c27bb49bebd416306e8014be1de3d
Duncan <dunc80@gmail.com>
2021-05-30 23:19:41 +0100

    make fonts optional and install Plug

--- setup.sh
+++ setup.sh
@@ -19,11 +19,21 @@ grep -q 'export FZF_DEFAULT_COMMAND="rg --files"' ~/.bashrc \
 grep -q 'export FZF_DEFAULT_OPTS="-m --height 50% --border"' ~/.bashrc \
 	||  sed -i '2iexport FZF_DEFAULT_OPTS="-m --height 50% --border"' ~/.bashrc
 
+echo "would you like to install fonts?"
+echo "type y for yes"
+echo ""
+read reply
+case $reply in
+
+y)
 #install nerdfont
 cd ~/Downloads
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Noto.zip
 unzip Noto.zip -d ~/.fonts
 fc-cache -fv
+;;
+
+esac
 
 # move existing .vimrc
 mv ~/.config/nvim/init.vim ~/.config/nvim/init.vim.backup 
@@ -37,5 +47,7 @@ mkdir ~/.config/nvim/after/
 mkdir ~/.config/nvim/after/plugin/
 ln -s ~/vim/after.vim ~/.config/nvim/after/plugin/after.vim
 
+# install vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 #open vim and run initial setup function
 nvim -c"call Setup()"
